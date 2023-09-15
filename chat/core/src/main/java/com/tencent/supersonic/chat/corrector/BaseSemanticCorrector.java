ddeb8fb9df2a77b2c46dd6acdd62517cbbe6720f
ychy00001 <yangchunyu00001@163.com>
2023-09-15 10:06:58 +0800

    add python dot_env update mysql driver

--- chat/core/src/main/java/com/tencent/supersonic/chat/corrector/BaseSemanticCorrector.java
+++ chat/core/src/main/java/com/tencent/supersonic/chat/corrector/BaseSemanticCorrector.java
@@ -6,15 +6,19 @@ import com.tencent.supersonic.chat.api.pojo.SemanticSchema;
 import com.tencent.supersonic.common.util.ContextUtils;
 import com.tencent.supersonic.knowledge.service.SchemaService;
 import com.tencent.supersonic.semantic.api.model.enums.TimeDimensionEnum;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 
 @Slf4j
 public abstract class BaseSemanticCorrector implements SemanticCorrector {
     public static final String DATE_FIELD = "数据日期";
+
     protected Map<String, String> getFieldToBizName(Long modelId) {
 
         SemanticSchema semanticSchema = ContextUtils.getBean(SchemaService.class).getSemanticSchema();
@@ -22,6 +26,21 @@ public abstract class BaseSemanticCorrector implements SemanticCorrector {
         List<SchemaElement> dbAllFields = new ArrayList<>();
         dbAllFields.addAll(semanticSchema.getMetrics());
         dbAllFields.addAll(semanticSchema.getDimensions());
+
+        // add Alias
+        List<SchemaElement> aliaFields = new ArrayList<>();
+        dbAllFields.stream()
+                .filter(item -> item.getAlias() != null && item.getAlias().size() > 0)
+                .forEach(item -> {
+                    List<String> alias = item.getAlias();
+                    for (String alia : alias) {
+                        SchemaElement aliaElement = new SchemaElement();
+                        BeanUtils.copyProperties(item, aliaElement);
+                        aliaElement.setName(alia);
+                        aliaFields.add(aliaElement);
+                    }
+                });
+        dbAllFields.addAll(aliaFields);
 
         Map<String, String> result = dbAllFields.stream()
                 .filter(entry -> entry.getModel().equals(modelId))
--- chat/core/src/main/python/llm/util/llm_instance.py
+++ chat/core/src/main/python/llm/util/llm_instance.py
@@ -3,21 +3,21 @@ from langchain.llms import OpenAI
 from provider.huggingface import HuggingFaceTextGenInference
 from langchain.llms import AzureOpenAI
 from run_config import MODEL_NAME, OPENAI_API_KEY, TEMPERATURE
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 import json
 
-load_dotenv()
+config=dotenv_values()
 
 # llm = OpenAI(openai_api_key=OPENAI_API_KEY, model_name=MODEL_NAME,
 #              temperature=TEMPERATURE)
 llm = AzureOpenAI(
-        openai_api_key=OPENAI_API_KEY,
-        openai_api_base=OPENAI_API_BASE,
-        openai_api_type=OPENAI_API_TYPE,
-        openai_api_version=OPENAI_API_VERSION,
-        deployment_name=AZURE_DEPLOYMENT_NAME,
-        temperature=OPENAI_API_TEMPERATURE,
-        stop=json.loads(OPENAI_API_STOP)
+        openai_api_key=config['OPENAI_API_KEY'],
+        openai_api_base=config['OPENAI_API_BASE'],
+        openai_api_type=config['OPENAI_API_TYPE'],
+        openai_api_version=config['OPENAI_API_VERSION'],
+        deployment_name=config['AZURE_DEPLOYMENT_NAME'],
+        temperature=config['OPENAI_API_TEMPERATURE'],
+        stop=json.loads(config['OPENAI_API_STOP'])
     )
 
 
