5f6e6a41d6ee31c0ca2581f03cdf985303de546c
ychy00001 <yangchunyu00001@163.com>
2023-11-06 11:30:11 +0800

    add server build shell

--- webapp/run_dev.sh
+++ webapp/run_dev.sh
@@ -1,2 +1,4 @@
 cd ./packages/supersonic-fe
+pnpm link ../chat-sdk
+pnpm i
 pnpm start
---
+++ webapp/server_build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm -rf pnpm-lock.yaml pnpm-lock.yaml.back
+rm -rf ./packages/supersonic-fe/src/.umi ./packages/supersonic-fe/src/.umi-production
+rm -rf node_modules ./packages/supersonic-fe/node_modules  ./packages/chat-sdk/node_modules
+cd ./packages/chat-sdk
+chown -R 777 /data/project/supersonic/webapp/
+pnpm i
+chown -R 777 /data/project/supersonic/webapp/
+pnpm run build --unsafe-perm=true --allow-root
+pnpm link --global
